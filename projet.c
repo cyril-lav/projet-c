69adf1c663cdf490d32866cf18b84b221014078d
NaoZao <denis.greffet42840@gmail.com>
2019-12-17 18:51:00 +0100

    modif projet.c et logements.don

--- projet.c
+++ projet.c
@@ -1,1 +1,13 @@
-#include"projet.h"
+#include "projet.h"
+
+// J'adore Cyril
+
+// 3)
+void affichLogeOccup(Logement tabLoge[], int nbLoge){
+	int i;
+	for(i=0;i<nbLoge;i++){
+		if(tabLoge[0].idEtudOccup!=NULL){ // mettre * si tableau de pointeurs
+			printf("%s\t%s\t%s\t%d\t%d\t%s\n",tabLoge[i].idLoge, tabLoge[i].cite, tabLoge[i].type, tabLoge[i].handicapAdapte, tabLoge[i].dispo, tabLoge[i].idEtudOccup);
+}
+
+
