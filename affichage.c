bda8a860c197bc22c3a0a2ad6f056f7d2c3dd6ab
NaoZao <root@DebianDenis>
2020-01-07 17:06:07 +0100

    gcc marche pas jsp pk

--- affichage.c
+++ affichage.c
@@ -9,64 +9,64 @@
 
 // Affichage menu
 void menu(void){
-printf("\n###############################################\n");
-printf("#  1-Afficher Logements Disponibles           #\n");
-printf("#  2-Afficher Logements Occupés               #\n");
-printf("#  3-Afficher Demandes Logements en Attente   #\n");
-printf("#  4-Attribuer Demandes à des Logements       #\n");
-printf("#  5-Création Demande Logement                #\n");
-printf("#  6-Annulation Demande Logement              #\n");
-printf("#  7-Libération Logement -> Attribution Auto  #\n");
-printf("#  8-Sauvegarde                               #\n");
-printf("#  9-Quitter                                  #\n");
-printf("###############################################\n\n");
+  printf("\n###############################################\n");
+  printf("#  1-Afficher Logements Disponibles           #\n");
+  printf("#  2-Afficher Logements Occupés               #\n");
+  printf("#  3-Afficher Demandes Logements en Attente   #\n");
+  printf("#  4-Attribuer Demandes à des Logements       #\n");
+  printf("#  5-Création Demande Logement                #\n");
+  printf("#  6-Annulation Demande Logement              #\n");
+  printf("#  7-Libération Logement -> Attribution Auto  #\n");
+  printf("#  8-Sauvegarde                               #\n");
+  printf("#  9-Quitter                                  #\n");
+  printf("###############################################\n\n");
 }
 
 
-void triBulle(char tab[30],int nb){
-	int i, cpt=0;
-	char tabReserve[30];
-	if(nb==0 || nb==1) return;
-	for(i=0;i<nb-1;i++){
-		if(strcmp(tab[i],tab[i+1])>=0){
-		strcpy(tabReserve,tab[i]);
-		strcpy(tab[i+1],tab[i];
-		strcpy(tab[i],tabReserve);
-		cpt++;
-		} // ou cpt++ ici je sais plus
-	}
-	if(cpt!=0){
-		triBulle(tab,nb-1);
-	}
+void triBulle(Logement* *tabLoge,int nb){
+  int i, cpt=0;
+  Logement* logementReserve;
+  if(nb==0 || nb==1) return;
+  for(i=0;i<nb-1;i++){
+    if(strcmp(tabLoge[i]->cite,tabLoge[i+1]->cite)>0){
+      logementReserve=tabLoge[i+1];
+      tabLoge[i+1]=tabLoge[i];
+      tabLoge[i]=logementReserve;
+      cpt++;
+    }
+  }
+  if(cpt!=0){
+    triBulle(tabLoge,nb-1);
+  }
 }
 
-void affichLogeOccup(Logement *tabLoge[], int nbLoge){
-	int i;
-	triBulle(tabLoge,nbLoge);
-	for(i=0;i<nbLoge;i++){
-		if(tabLoge[i]->idEtudOccup!=NULL){ // mettre * si tableau de pointeurs
-			printf("%s\t%s\t%s\t%d\t%d\t%s\n",tabLoge[i]->idLoge, tabLoge[i]->cite, tabLoge[i]->type, tabLoge[i]->handicapAdapte, tabLoge[i]->dispo, tabLoge[i]->idEtudOccup);
-		}
-	}
+void affichLogeOccup(Logement* *tabLoge, int nbLoge){
+  int i;
+  triBulle(tabLoge,nbLoge);
+  for(i=0;i<nbLoge;i++){
+    if(tabLoge[i]->idEtudOccup!=NULL){
+      printf("%s\t%s\t%s\t%d\t%d\t%s\n",tabLoge[i]->idLoge, tabLoge[i]->cite, tabLoge[i]->type, tabLoge[i]->handicapAdapte, tabLoge[i]->dispo, tabLoge[i]->idEtudOccup);
+    }
+  }
 }
 
 // Menu -> 2)
 /*void affichLogeOccup(Logement *tabLoge[], int nbLoge){
-	int i;
-	for(i=0;i<nbLoge;i++){
-		if(tabLoge[i]->idEtudOccup!=NULL){ // mettre * si tableau de pointeurs
-			printf("%s\t%s\t%s\t%d\t%d\t%s\n",tabLoge[i]->idLoge, tabLoge[i]->cite, tabLoge[i]->type, tabLoge[i]->handicapAdapte, tabLoge[i]->dispo, tabLoge[i]->idEtudOccup);
-		}
-	}
-}
+  int i;
+  for(i=0;i<nbLoge;i++){
+  if(tabLoge[i]->idEtudOccup!=NULL){ // mettre * si tableau de pointeurs
+  printf("%s\t%s\t%s\t%d\t%d\t%s\n",tabLoge[i]->idLoge, tabLoge[i]->cite, tabLoge[i]->type, tabLoge[i]->handicapAdapte, tabLoge[i]->dispo, tabLoge[i]->idEtudOccup);
+  }
+  }
+  }
 */
 
 // Menu -> 1)
-void affichLogeDispo(Logement *tabLoge[], int nbLoge){
-	int i;
-	for(i=0;i<nbLoge;i++){
-		if(tabLoge[i]->idEtudOccup==NULL){ // mettre * si tableau de pointeurs
-			printf("%s\t%s\t%s\t%d\t%d\n",tabLoge[i]->idLoge, tabLoge[i]->cite, tabLoge[i]->type, tabLoge[i]->handicapAdapte, tabLoge[i]->dispo);
-		}
-	}
+void affichLogeDispo(Logement* *tabLoge, int nbLoge){
+  int i;
+  for(i=0;i<nbLoge;i++){
+    if(tabLoge[i]->idEtudOccup==NULL){ // mettre * si tableau de pointeurs
+      printf("%s\t%s\t%s\t%d\t%d\n",tabLoge[i]->idLoge, tabLoge[i]->cite, tabLoge[i]->type, tabLoge[i]->handicapAdapte, tabLoge[i]->dispo);
+    }
+  }
 }
--- main.c
+++ main.c
@@ -2,67 +2,76 @@
 #include "projet.h"
 
 void appli(void){
-	char choix='0'; // Choix dans le menu
-	char confirmQuit='X';
-	char saveDone='0';
-	FILE *feLoge;
-	Logement *tabLoge[TAILLE];
-	int nbLoge;
-	feLoge=fopen("logements.don","r");
-	if(feLoge==NULL){
-		printf("Erreur ouverture fichier \"logements.don\"\n");
-		return;
-	}
+  char choix='0'; // Choix dans le menu
+  char confirmQuit='X';
+  char saveDone='0';
+  FILE *feLoge;
+  Logement *tabLoge[TAILLE];
+  int nbLoge;
+  feLoge=fopen("logements.don","r");
+  if(feLoge==NULL){
+    printf("Erreur ouverture fichier \"logements.don\"\n");
+    return;
+  }
 
-	nbLoge=chargeLogement(tabLoge,TAILLE,feLoge);
+
 	
-	menu();
-	scanf("%c%*c",&choix);
+  menu();
+  scanf("%c%*c",&choix);
 	while(!(choix=='9' && saveDone=='1')){
-		switch(choix){
-			case '1':
-				affichLogeDispo(tabLoge, nbLoge);
-			case '2':
-				affichLogeOccup(tabLoge, nbLoge);
-			case '3':
-				break;
-			case '4':
-				saveDone='0';
-				break;
-			case '5':
-				saveDone='0';
-				break;
-			case '6':
-				saveDone='0';
-				break;
-			case '7':
-				saveDone='0';
-				break;
-			case '8':
-				saveDone='1';
-				break;
-			case '9':
-				if(saveDone=='0'){
-					printf("Vous n'avez pas sauvegardé, voulez-vous quand même quitter l'application ? (O/N)\n");
-					scanf("%c%*c",&confirmQuit);
-					if(confirmQuit=='O' || confirmQuit=='o'){
-						saveDone='1';
-					}
-				}
-				break;
-			default:
-				printf("Erreur: valeur non valide\n");
-		}
-		if(choix=='9' && saveDone=='1'){
-			printf("La bise <3\n");
-			return;
-		}
-		menu();
-		scanf("%c%*c",&choix);
+	  switch(choix){
+	  case '1':
+	    affichLogeDispo(tabLoge, nbLoge);
+	  case '2':
+	    affichLogeOccup(tabLoge, nbLoge);
+	  case '3':
+	    break;
+	  case '4':
+	    saveDone='0';
+	    break;
+	  case '5':
+	    saveDone='0';
+	    break;
+	  case '6':
+	    saveDone='0';
+	    break;
+	  case '7':
+	    saveDone='0';
+	    break;
+	  case '8':
+	    saveDone='1';
+	    break;
+	  case '9':
+	    if(saveDone=='0'){
+	      printf("Vous n'avez pas sauvegardé, voulez-vous quand même quitter l'application ? (O/N)\n");
+	      scanf("%c%*c",&confirmQuit);
+	      if(confirmQuit=='O' || confirmQuit=='o'){
+		saveDone='1';
+	      }
+	    }
+	    break;
+	  default:
+	    printf("Erreur: valeur non valide\n");
+	  }
+	  if(choix=='9' && saveDone=='1'){
+	    printf("La bise <3\n");
+	    return;
+	  }
+	  menu();
+	  scanf("%c%*c",&choix);
 	}
 	return;
 }
 
 int main(void){
-	appli();
+  int nbLoge;
+  Logement tabLoge[TAILLE];
+  FILE *fe;
+  fe=fopen("logements.don","r");
+  if(fe==NULL){
+    printf("pb ouv fichier\n");
+    return -1;
+  }
+  nbLoge=chargeLogement(tabLoge,100,fe);
+  // appli();
 }
